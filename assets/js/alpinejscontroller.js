df9cf0133b3aad1874798475c5e72398409d7818
parham <parhamfard5@gmail.com>
2024-11-27 17:16:24 +0330

    part19

--- assets/js/alpinejscontroller.js
+++ assets/js/alpinejscontroller.js
@@ -2,9 +2,13 @@ document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', function(){
         return {
             users: [],
+            isloading: false,
             getUsers(){
+                this.isloading = true
                 axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                     this.users= res.data
+                    this.isloading = false
+                    console.log(res);
                 })
             }
        }
